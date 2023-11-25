0bb453e8ad7e0147a233ebbfac5dda5e97e766cd
Mostakimul Karim <mostakimulkarim@gmail.com>
2023-11-25 02:40:37 -0500

    feat: user model created

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,23 @@
+import { Schema, model } from 'mongoose';
+import { TUser } from './user.interface';
+
+const UserSchema = new Schema<TUser>({
+  userId: { type: Number, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  fullName: {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+  },
+  age: { type: Number, required: true },
+  email: { type: String, required: true },
+  isActive: { type: Boolean, required: true },
+  hobbies: [{ type: String }],
+  address: {
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+  },
+});
+
+export const UserModel = model<TUser>('User', UserSchema);
